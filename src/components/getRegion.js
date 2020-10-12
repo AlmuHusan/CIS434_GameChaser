bd7ba764b022a62ff5db72427366396eb0b05562
ctrl-dan <willsd112@gmail.com>
2020-10-12 14:42:12 -0400

    Updated getRegion

--- src/components/getRegion.js
+++ src/components/getRegion.js
@@ -6,22 +6,14 @@ import React from 'react';
         ip : null,
         timezone : null,
       };
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> DansWork
       async componentDidMount() {
         const url = "https://geo.ipify.org/api/v1?apiKey=at_gFewbGeplb0gcg0S6HynNZZA5N6gk&ipAddress=";
         const response = await fetch(url);
         const data = await response.json();
         this.setState({ ip: data.ip, loading: false, timezone: data.location.timezone});
       }
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> DansWork
       render() {
         if (this.state.loading) {
           return <div>loading...</div>;
@@ -38,14 +30,8 @@ import React from 'react';
         if(this.state.timezone == "-07:00" || "-06:00"){
             return <div>West Coast</div>
         }
-<<<<<<< HEAD
-        
-      }
-}
-export default GetRegion
-=======
+
 
       }
 }
 export default GetRegion
->>>>>>> DansWork
