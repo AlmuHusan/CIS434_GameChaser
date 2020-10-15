50fa0711ada6e474213647b572ddcb4d14700693
UdomP <sereyudomp@gmail.com>
2020-10-15 16:20:46 -0400

    Revert "some fixes"
    This reverts commit 4589b4fec7af7ef988791f06709159392e3a85df.

--- src/components/getRegion.js
+++ src/components/getRegion.js
@@ -19,16 +19,16 @@ import React from 'react';
           return <div>loading...</div>;
         }
         if (!this.state.ip) {
-          return <div>didn't get location</div>;
+          return <div>didn't get a ip</div>;
         }
-        if(this.state.timezone == "-04:00" || "-03:00" || "-05:00" || "-06:00" || "-07:00" || "-02:00"){
-            return <div>NA</div>
+        if(this.state.timezone == "-04:00"){
+            return <div>East Coast</div>
         }
-        if(this.state.timezone == "+01:00" || "+02:00" || "+03:00" || "+04:00" || "+05:00" || "+06:00"){
-            return <div>EU</div>
+        if(this.state.timezone == "-05:00"){
+            return <div>Central</div>
         }
-        if(this.state.timezone == "+07:00" || "+08:00" || "+09:00" || "+10:00" || "+11:00" || "+12:00"){
-            return <div>Asia</div>
+        if(this.state.timezone == "-07:00" || "-06:00"){
+            return <div>West Coast</div>
         }
 
 
