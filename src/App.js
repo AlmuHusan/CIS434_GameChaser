98caf2f528011e39dcb64bf23e20dc91d4cf886e
UdomP <sereyudomp@gmail.com>
2020-10-15 17:53:17 -0400

    Revert "Revert "Revert "Merge branch 'master' into Udom"""
    This reverts commit 67f24cbe9eaf11075c3941f41d71dcd078f789a1.

--- src/App.js
+++ src/App.js
@@ -2,87 +2,80 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import './App.css';
-import Login from './Login.js';
-import Home from './Home.js';
-import Blank from './blank.js';
-import Registration from './Registration.js';
-import GetRegion from './components/getRegion';
+import GetRegion from './Component/getRegion';
 
+function btPress() {
+    alert('Code to do here!');
+}
 
 
 class App extends React.Component{
 
   constructor(props) {
-          super(props);
-          this.state = {
-              loaded: false,
-
-
-          };
-
-
-      }
-      componentDidMount = async () => {
-          this.setState({ loaded: true })
-      }
-
-
+    super(props);
+    this.state = {
+      loaded: false,
+      region: <GetRegion/>,
+    };  
+  }
+  componentDidMount= async()=>{
+    this.setState({loaded:true})
+}
   render(){
-
-
-
+    const items = this.props.tableInfo;
+    
+    const itemList = items.map((items,index) =>
+    
+    <tr key={index}>
+    <td>{items.Game.toString()}</td>
+    <td>{items.Name.toString()}</td>
+    <td>{items.Size.toString()}</td>
+    <td>{items.Time.toString()}</td>
+    <td>{items.Notes.toString()}</td>
+  </tr>
+  );
   return (
-
+  	
     <div className="App">
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="home">Game Chasers</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">About</Nav.Link>
-          <NavDropdown title="Region" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="3.1">NA</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="3.2">SA</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="3.3">Asia</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="3.4">EU</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link eventKey={2} href="login">
-            Login
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+          <div className= "App-header" >
+                <h1 style={{display:"inline", alignItems:"center"}}> GAME CHASERS!</h1>
+                <h1 style={{display:"inline", alignItems:"center"}}>{this.state.region}</h1> 
+                
 
+           
+          </div>{/*End of header*/}
 
-    <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/registration" component={Registration} />
-        <Route exact path="/home" component={() => <Home  tableInfo={this.props.tableInfo} />} />
-      </Switch>
+          <div className="App-searchbar">
+             
+            <a >Search by game</a>
+            <input type="text" />
+              &emsp;
+            <a >Search by user</a>
+            <input type="text" />
+              &emsp;
+            <Button variant="secondary" size="lg" onClick={btPress} > Search </Button>{''}
+          </div> {/* End of searchbar */}
 
-    </Router>
-
-
+          <div className="App-table">
+            <table style={{width:"100%",border: "1px solid black",borderCollapse: "collapse"}} >
+                <tr>
+                  <th>Game</th>
+                  <th>Name</th>
+                  <th>Size</th>
+                  <th>Time</th>
+                  <th>Notes</th>
+                </tr>
+                {this.state.loaded ?
+                <React.Fragment >
+                    {itemList}
+                    </React.Fragment>
+                  :<div></div>
+                  }
+            </table> 
+          </div>
     </div>
   );
-
 }
-
 }
-
-
-
-
-
-
-
 export default App;
--- src/Component/getRegion.js
+++ src/Component/getRegion.js
@@ -1,37 +1,35 @@
 import React from 'react';
 
- class GetRegion extends React.Component{
+export default class getRegion extends React.Component{
       state = {
         loading: true,
         ip : null,
         timezone : null,
       };
-
+    
       async componentDidMount() {
         const url = "https://geo.ipify.org/api/v1?apiKey=at_gFewbGeplb0gcg0S6HynNZZA5N6gk&ipAddress=";
         const response = await fetch(url);
         const data = await response.json();
         this.setState({ ip: data.ip, loading: false, timezone: data.location.timezone});
       }
-
+    
       render() {
+        
         if (this.state.loading) {
-          return <div>loading...</div>;
+          return <div>loading...  </div>;
         }
         if (!this.state.ip) {
-          return <div>didn't get a ip</div>;
+          return <div>didn't get a ip  </div>;
         }
-        if(this.state.timezone == "-04:00"){
-            return <div>East Coast</div>
+        if(this.state.timezone == "-04:00" || "-03:00" || "-05:00" || "-06:00" || "-07:00" || "-02:00"){
+            return <div>NA</div>
         }
-        if(this.state.timezone == "-05:00"){
-            return <div>Central</div>
+        if(this.state.timezone == "+01:00" || "+02:00" || "+03:00" || "+04:00" || "+05:00" || "+06:00"){
+            return <div>EU</div>
         }
-        if(this.state.timezone == "-07:00" || "-06:00"){
-            return <div>West Coast</div>
-        }
-
-
+        if(this.state.timezone == "+07:00" || "+08:00" || "+09:00" || "+10:00" || "+11:00" || "+12:00"){
+            return <div>Asia</div>
+      }
       }
 }
-export default GetRegion
