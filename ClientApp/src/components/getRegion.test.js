174dcebda9c3760d3e871f8b321ca6874fa443d0
Jeff <71464201+Jellyon@users.noreply.github.com>
2020-11-15 18:37:07 -0500

    Add files via upload

---
+++ ClientApp/src/components/getRegion.test.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, cleanup } from '@testing-library/react';
+import App from './../App';
+import './getRegion.js';
+import GetRegion from './getRegion.js';
+
+afterEach(cleanup);
+it('succesfully gets Region', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<GetRegion />, div);
+});
