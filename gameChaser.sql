ae0fbe35f4ebc63084cc1cfa73c6457e1be0c107
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2020-11-24 15:56:21 -0500

    added Region

--- gameChaser.sql
+++ gameChaser.sql
@@ -5,11 +5,11 @@ CREATE TABLE GameSessions (
     Size INTEGER,
 	Time DateTime,
     Summary varchar(255),
-	TimeZone varchar(255),
+	Region varchar(255),
 	PRIMARY KEY (GameSessionId)
 );
 
 INSERT INTO GameSessions VALUES
-(1,'Mario Sunshine','luigi55',2,'2008-11-11','Test','East Coast'),
+(1,'Mario Sunshine','luigi55',2,'2008-11-11','Test','USA'),
 (2,'League of Legends','MikeEagle',5,'2008-11-11','Gold rank or higher only','Asia'),
 (3,'Mario BuhBye','luigi55',3,'2008-11-11','late night casuals','Europe')
