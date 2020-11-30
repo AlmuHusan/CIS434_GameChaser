fd0a2c2a7266fa6aa4261ea31c024abbb431ecce
ctrl-dan <71346088+ctrl-dan@users.noreply.github.com>
2020-11-29 19:52:07 -0500

    Revert "Merge branch 'master' of https://github.com/AlmuHusan/CIS434_GameChaser"
    This reverts commit 5893e6299a6803af2d6d7b52616e537cec4fc8c7, reversing
    changes made to defffea082708d0625e0314df160b65216ca5ea6.

--- gameChaser.sql
+++ gameChaser.sql
@@ -11,7 +11,6 @@ CREATE TABLE GameSessions (
 
 INSERT INTO GameSessions VALUES
 
-(1,'Mario Sunshine','luigi55',2,'2008-11-11','Test','NA'),
+(1,'Mario Sunshine','luigi55',2,'2008-11-11','Test','East Coast'),
 (2,'League of Legends','MikeEagle',5,'2008-11-11','Gold rank or higher only','Asia'),
-(3,'Mario BuhBye','luigi55',3,'2008-11-11','late night casuals','EU'),
-(3,'Pain Train','evilclown22',99,'2008-12-12','PAIN','NA')
+(3,'Mario BuhBye','luigi55',3,'2008-11-11','late night casuals','Europe')
